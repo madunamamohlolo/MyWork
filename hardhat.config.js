2a18bd586586ee3e9d900b86cb7cb2623b407cee
madnamamohlolo <madunamamohlolo@gmail.com>
2024-12-05 18:34:30 +0200

    DECENTRALIZED PRODUCT PROJECT

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY], // Wrap the private key in an array
+    },
+  },
+};
